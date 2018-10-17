74f253b1faa2a2d79a5194f5829eb5b009a797f6
lgrgic <lukagrgic14@gmail.com>
2018-10-17 13:14:13 +0200

    Finish working and refactored palindrome method

--- lib/lgrgic_palindrome_detector.rb
+++ lib/lgrgic_palindrome_detector.rb
@@ -1,5 +1,16 @@
 require "lgrgic_palindrome_detector/version"
 
-module LgrgicPalindromeDetector
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.scan(/[a-z]/i).join.downcase
+    end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "lgrgic_palindrome_detector"
-
 require "minitest/autorun"
+require "minitest/reporters"
+Minitest::Reporters.use!
