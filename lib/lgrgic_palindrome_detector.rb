cddd8d0275866d8d6acd3945addfc73dd214799c
lgrgic <lukagrgic14@gmail.com>
2018-10-17 12:54:05 +0200

    Initialize repository

---
+++ lib/lgrgic_palindrome_detector.rb
@@ -0,0 +1,5 @@
+require "lgrgic_palindrome_detector/version"
+
+module LgrgicPalindromeDetector
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "lgrgic_palindrome_detector"
+
+require "minitest/autorun"
